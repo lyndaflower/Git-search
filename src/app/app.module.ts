70d54497d3fc75d52b2ddbc61bb3c7accb5e0bab
lyndaflower <umurerwalynda@gmail.com>
2019-08-30 15:24:18 +0200

    add api component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiComponent } from './api/api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiComponent
   ],
   imports: [
     BrowserModule,
